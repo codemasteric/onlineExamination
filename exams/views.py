0b8d8c779d2d647347c5a92a9819de6e7bdb2cf3
abramoken <abioken33@gmail.com>
2020-08-19 12:07:36 +0300

    error on retreiving random questions

--- exams/views.py
+++ exams/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-# from django.http import HttpResponseRedirect
+from django.db.models import Max
 from django.contrib import messages
-from random import randint
+import random
 from django.db import connection
 
 from .models import Instruction, Question, Answer, Result
@@ -14,19 +14,24 @@ def instruction(request):
     return render(request, 'exams/instruction.html', context)
 
 def question(request):
-    questions = Question.objects.all().order_by('?')[:3]
+    max_id = Question.objects.all().aggregate(max_id=Max("id"))['max_id']
+    while True:
+        pk = random.randint(1, max_id)
+        questions = Question.objects.filter(pk=pk).first()
+        if questions:
+            return questions
     content = {
         'questions': questions
     }
     if request.method == 'POST':
 
         option_1 = request.POST.get('option_1', " ")
-        option_2 = request.POST.get('option_2', False)
-        option_3 = request.POST.get('option_3', False)
-        option_4 = request.POST.get('option_4', False)
-        option_5 = request.POST.get('option_5', False)
-        question_text = request.POST.get('question_text', False)
-        correct_option = request.POST.get('correct_option', False)
+        option_2 = request.POST.get('option_2', " ")
+        option_3 = request.POST.get('option_3', " ")
+        option_4 = request.POST.get('option_4', " ")
+        option_5 = request.POST.get('option_5', " ")
+        question_text = request.POST.get('question_text', " ")
+        correct_option = request.POST.get('correct_option', " ")
         
         savedata = Result(option_1=option_1,option_2=option_2,option_3=option_3,
                             option_4=option_4,option_5=option_5,question_text=question_text,correct_option=correct_option)
