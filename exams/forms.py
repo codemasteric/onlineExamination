671a45bbd1ad93b60071f684828c46754538af4d
codemasteric <onenfelix90@gmail.com>
2020-08-20 11:43:05 +0300

    Added ability to retrieve one random non-repeating questions from the model

--- exams/forms.py
+++ exams/forms.py
@@ -1,14 +1,14 @@
-# from django import forms
+from django import forms
 
-# from .models import Question, Answer, Result
+from .models import Question, Answer, Result
 
-# class QuestionForm(forms.ModelForm):
-#     option_1 = forms.CharField(max_length=200)
-#     option_2 = forms.CharField(max_length=200)
-#     option_3 = forms.CharField(max_length=200)
-#     option_4 = forms.CharField(max_length=200)
-#     option_5 = forms.CharField(max_length=200)
-#     correct_option = forms.CharField(max_length=200)
-#     class Meta:
-#         model = Result
-#         fields = ('option_1', 'option_2', 'option_3', 'option_4', 'option_5', 'correct_option')
+class QuestionForm(forms.ModelForm):
+    option_1 = forms.CharField(max_length=200)
+    option_2 = forms.CharField(max_length=200)
+    option_3 = forms.CharField(max_length=200)
+    option_4 = forms.CharField(max_length=200)
+    option_5 = forms.CharField(max_length=200)
+    correct_option = forms.CharField(max_length=200)
+    class Meta:
+        model = Result
+        fields = ('option_1', 'option_2', 'option_3', 'option_4', 'option_5', 'correct_option')
--- exams/views.py
+++ exams/views.py
@@ -15,29 +15,31 @@ def instruction(request):
 
 def question(request):
     max_id = Question.objects.all().aggregate(max_id=Max("id"))['max_id']
+    questions = []
     while True:
         pk = random.randint(1, max_id)
-        questions = Question.objects.filter(pk=pk).first()
-        if questions:
-            return questions
+        question = Question.objects.filter(pk=pk).first()
+        if question:
+            questions.append(question)
+            break
     content = {
         'questions': questions
     }
-    if request.method == 'POST':
+    # if request.method == 'POST':
 
-        option_1 = request.POST.get('option_1', " ")
-        option_2 = request.POST.get('option_2', " ")
-        option_3 = request.POST.get('option_3', " ")
-        option_4 = request.POST.get('option_4', " ")
-        option_5 = request.POST.get('option_5', " ")
-        question_text = request.POST.get('question_text', " ")
-        correct_option = request.POST.get('correct_option', " ")
+    #     option_1 = request.POST.get('option_1', " ")
+    #     option_2 = request.POST.get('option_2', " ")
+    #     option_3 = request.POST.get('option_3', " ")
+    #     option_4 = request.POST.get('option_4', " ")
+    #     option_5 = request.POST.get('option_5', " ")
+    #     question_text = request.POST.get('question_text', " ")
+    #     correct_option = request.POST.get('correct_option', " ")
         
-        savedata = Result(option_1=option_1,option_2=option_2,option_3=option_3,
-                            option_4=option_4,option_5=option_5,question_text=question_text,correct_option=correct_option)
+    #     savedata = Result(option_1=option_1,option_2=option_2,option_3=option_3,
+    #                         option_4=option_4,option_5=option_5,question_text=question_text,correct_option=correct_option)
         
-        savedata.save()
-        messages.success(request, 'Submision Successiful')
-        return redirect('question')        
-    else:
-        return render(request, 'exams/question.html', content)
+    #     savedata.save()
+    #     messages.success(request, 'Submision Successiful')
+    #     return redirect('question')        
+    # else:
+    return render(request, 'exams/question.html', content)
