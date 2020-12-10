52e62847dd728d79d2e76a787d0cc87f4e8772cf
codemasteric <41568873+codemasteric@users.noreply.github.com>
2020-12-10 16:41:12 +0300

    Update settings.py

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9upa7f(bvo7-cxl%&k8x&$)trn^+!gl8jcc7et-*mj7$kh9!n%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
